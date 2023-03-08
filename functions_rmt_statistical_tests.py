78ae02b544ee1e07907e605c6afdfdd2319a1972
Andres <andgarm.n@gmail.com>
2023-03-08 10:38:52 -0600

    Add files via upload

---
+++ functions_rmt_statistical_tests.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 13:45:23 2022
+
+@author: andres
+"""
+
+
+### librerias
+import numpy as np
+from scipy.linalg import eigh
+import pandas as pd
+import os
+
+
+#######################################
+path = os.getcwd()
+significancias_onatski = pd.read_csv(path+'/data/tabla_onatski.csv',sep=',',\
+                                     dtype=str,encoding='latin1',index_col=0)
+    
+significancias_tw = pd.read_csv(path+'/data/tabla_tracy_widom.csv',sep=' ',\
+                                     header=None,dtype=str,encoding='latin1')
+
+
+#######################################
+### Algunas funciones utiles para limpieza de datos
+def checkIfDuplicates(listOfElems):
+    ''' Check if given list contains any duplicates '''    
+    for elem in listOfElems:
+        if listOfElems.count(elem) > 1:
+            return elem
+    return False
+
+class my_dictionary(dict): 
+
+    def __init__(self): 
+        self = dict() 
+          
+    def add(self, key, value): 
+        self[key] = value 
+
+
+def preproceso(data):
+    data = data-data.mean(axis=0)
+    data = data/data.std(axis=0)
+    return(data)
+
+def cov2corr(S):
+    p = len(S)
+    C = np.zeros([p,p])
+    for i in range(p):
+        for j in range(i,p):
+            C[i,j] = S[i,j]/(np.sqrt(S[i,i])*np.sqrt(S[j,j]))
+            C[j,i] = C[i,j]
+            
+    return(C)
+
+
+def axis(period, data):
+    dates = []
+    period = period
+    data = data
+    n = data.shape[0]
+    index = data.index.to_list()
+    for k in np.arange(0, n,period):
+        dates.append(index[k])     
+    return(n, period, dates)
+
+#### Imputación de datos
+"""
+ 1) Se eliminan las series de precios con mas de un cierto porcentaje de dias faltantes.
+ 2) Las series restantes se imputan con splines de orden 3.
+ 3) Se calculan retornos logartimicos
+"""
+
+def imputation(close, ratio=1):
+    df_dataset = close
+    if isinstance(close, pd.DataFrame)==True:
+        n,p = df_dataset.shape
+        markets = df_dataset.columns
+        percentage = ratio
+        dataset_imputed = my_dictionary() 
+        for i in range(p):
+            market = markets[i]
+            if df_dataset[market].max() > 50000:
+                 continue
+            missing = df_dataset[market].isnull().sum()
+            if  missing <= percentage*n/100:
+                dataset_imputed.add(market, df_dataset[market])              
+    
+        prices_dataset_imputed = pd.DataFrame(dataset_imputed)  
+        prices_dataset_imputed = prices_dataset_imputed.set_index(np.linspace(0,n-1,n))     
+        prices_dataset_imputed = prices_dataset_imputed.astype(float).interpolate(method='linear', limit_direction='forward', order=3)
+        prices_dataset_imputed = prices_dataset_imputed.set_index(df_dataset.index)
+        returns = np.log(prices_dataset_imputed).diff().iloc[1:]
+        returns_imputed = returns.set_index(np.linspace(0,n-2,n-1))  
+        returns_imputed = returns_imputed.interpolate(method='linear', limit_direction='backward', order=3)
+        returns_imputed = returns_imputed.set_index(returns.index)
+        
+    
+    else:
+        prices_dataset_imputed = df_dataset
+        prices_dataset_imputed = prices_dataset_imputed.astype(float).interpolate(method='linear', limit_direction='forward', order=3)
+        returns = np.log(prices_dataset_imputed).diff().iloc[1:]
+        returns_imputed = returns.interpolate(method='linear', limit_direcion='backward', order=3)
+    return(prices_dataset_imputed,returns_imputed)
+
+#######################################
+### Funciones de matrices aleatorias
+
+### Marcenko-Pastur law
+def MP(q, points=1000):
+    dmin = (1.0 - np.sqrt(q))**2
+    dmax = (1.0 + np.sqrt(q))**2
+    t = np.linspace(dmin,dmax,points)
+    marcenko = np.zeros([len(t)])
+    for i in range(len(t)):
+        x = t[i]
+        if (x < dmin or x > dmax):
+            rho = 0
+        else:   
+            rho = np.sqrt((dmax-x)*(x-dmin))/(2*np.pi*q*x)
+        marcenko[i] = rho
+    return(t,marcenko)  
+
+
+### Bouchaud et. al. (2002) filtering techique
+def clipping_B(sample, T):
+    eigval, eigvec = eigh(sample) 
+    p = len(sample)
+    n = T
+    q = p/n
+    dmax = (1.0 + np.sqrt(q))**2
+    
+    #replace noise
+    noise = [] 
+    k=0
+    while (eigval[k] < dmax):
+        noise.append(eigval[k])
+        k+=1
+    if len(noise) > 0:
+        eignoise = np.mean(noise)
+    count=p-k
+
+    d = np.zeros([p])
+    for i in range(p):
+        if (eigval[i] < dmax):
+            d[i] = eignoise
+        else:
+            d[i] = eigval[i]
+
+    #reconstruct correlation matrix
+    D = np.diag(d)
+    XI_clipping = cov2corr(eigvec@D@eigvec.T)
+    
+    return(count,XI_clipping) 
+
+
+#######################################
+### Pruebas estadísitcas provenientes de RMT
+def onatski(data,k0):
+    #nota: version Onatski(2009)
+    n,p = data.shape
+    data1 = data.iloc[:int(n/2),:]
+    data2 = data.iloc[int(n/2):,:]
+    n,p = data1.shape
+    X = data1.values + data2.values*1j
+    WUE = pd.DataFrame((1/n)*X.conjugate().T@X) 
+    eigval_WUE, eigvec_GUE = eigh(WUE)
+
+    # Estadistico R
+    k1=7
+    k1_k0 = k1-k0+1
+    EIGS = eigval_WUE[::-1]
+    index=10
+    EIGENVALUES = np.zeros([index])
+    for i in range(index):
+        # Nota: el resultado no cambia al variar el centrado y escalamiento
+        EIGENVALUES[i] = (EIGS[i] - 2)*n**(-2/3)
+
+    R = np.zeros([k1_k0])
+    ii=0
+    for i in range(k0,k1+1):
+        statistics = (EIGENVALUES[i] - EIGENVALUES[i+1])/(EIGENVALUES[i+1] - EIGENVALUES[i+2])
+        if i==k0:
+            R[ii] = statistics
+        else:
+            R[ii] = max(R[ii-1],statistics)  
+        ii+=1
+    return(max(R))
+
+def test_onatski(TABLA, j=0, level = 1):
+    ### available levels: 1,2,3,4,5,6,7,8,9,10,15
+    logical = TABLA[str(j)].values > significancias_onatski.loc[level].astype(float).values
+    k = 0
+    for value in logical:
+        if  value==False:
+            number = k
+            break
+        k+=1
+        if k==8:
+            number = 8
+    return(number)
+
+
+### Funcion para encontrar probabilidades Tracy-Widom
+def twprob(twtable, x):
+    l = np.shape(twtable)[0]
+    i = 0
+    while(i < l and x >= twtable[i,0]):
+        i += 1
+    if(i == l):
+        return(0)
+    elif(i == 0):
+        return(1)
+    else:
+        return(twtable[i-1,1]+(twtable[i,1] - twtable[i-1,1])*(x - twtable[i-1,0]) / (twtable[i,0]-twtable[i-1,0]))
+
+
+### Probabilidad de encontrar lambda > lambda_1 (2do orden)
+def TW_Wishart_order2(p,n,twtable,lambda1,alpha):
+    mu = (np.sqrt(n-0.5) + np.sqrt(p-0.5))**2
+    sigma = np.sqrt(mu)*(1.0/np.sqrt(n-0.5)+1.0/np.sqrt(p-0.5))**(1./3)
+    x_lambda1 = (n*lambda1-mu)/sigma
+    probabilidad = twprob(twtable, x_lambda1)
+    return(probabilidad)
+
+def test_tw(eigval, n, alpha = 0.01):
+    twtable = significancias_tw.astype(float).values
+    p = len(eigval)
+    k = 0
+    for i in range(p):
+        lambda_1 = eigval[-(i+1)]
+        estadistico = TW_Wishart_order2(p,n,twtable, lambda_1,alpha)
+        if estadistico < alpha:
+            k +=1
+        else:
+            break
+    return(k)
+    
+    
+    
---
+++ main_rmt_statistical_tests.py
@@ -0,0 +1,344 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 13:44:32 2022
+
+@author: andres
+"""
+
+#######################################
+# libraries
+
+import numpy as np
+from scipy.linalg import eigh
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from functions_rmt_statistical_tests import checkIfDuplicates,preproceso,imputation,\
+    clipping_B,test_tw, onatski, test_onatski, MP, axis
+import time
+import json
+import statsmodels.api as sm
+import seaborn as sns
+
+#######################################
+### Parameters
+start_time = time.time()
+path = os.getcwd()
+market = 'nasdaq'
+case = 'high'
+percentage = 5
+start_date = '2005-01-03'
+end_date = '2022-12-30'
+#######################################
+### Data and preprocessing
+
+# Select market: S&P500 or NASDAQ
+if market == 'sp500':
+    index = 'S&P 500'
+    close = pd.read_pickle(path+'/data/sp500_2022.pkl')
+    MARKET = pd.read_csv(path+'/data/SPY.csv')
+    MARKET['Date'] = MARKET["Date"].apply(lambda x: pd.Series(str(x).split(" "))).iloc[:,0]
+    MARKET = MARKET.set_index(['Date'])
+elif market == 'nasdaq':
+    index = 'NASDAQ COMPOSITE'
+    close = pd.read_pickle(path+'/data/nasdaq_2022.pkl')
+    close.index = pd.to_datetime(close.index).strftime('%Y-%m-%d')
+    MARKET = pd.read_csv(path+'/data/IXIC.csv', sep=',')
+    MARKET = MARKET.set_index(['Date'])
+
+
+
+# Period of time
+close_window = close.loc[start_date:]
+close_market = MARKET['Close'].loc[start_date:]
+
+
+# Imputation
+prices_market, returns_market = imputation(close_market)
+prices_market = prices_market.loc[:end_date]
+returns_market = returns_market.loc[:end_date]
+prices, returns = imputation(close_window,ratio=percentage)
+prices = prices.loc[:end_date]
+returns = returns.loc[:end_date]
+
+
+T, N = returns.shape
+print(T,N, len(returns_market))
+
+
+# Check for duplicate companies
+tickers = returns.columns.to_list()
+duplicados = checkIfDuplicates(tickers)
+print(duplicados)
+
+
+# plotting preprocessed prices and returns
+plt.figure()
+length, period, dates = axis(252, prices_market)
+plt.plot(prices.values)
+plt.plot(prices_market.values,'k',linewidth=2, label=index)
+plt.yscale('log')
+plt.xticks(np.arange(0, length, period),dates) 
+plt.xticks(fontsize=10, rotation=90)
+plt.savefig(path+'/figures/prices_'+market+'.png',dpi=600, bbox_inches='tight')
+
+
+plt.figure()
+length, period, dates = axis(252,returns_market)
+plt.plot(returns.values)
+plt.plot(returns_market.values,'k',linewidth=2, label=index)
+plt.xticks(np.arange(0, length, period),dates) 
+plt.xticks(fontsize=10, rotation=90)
+plt.savefig(path+'/figures/returns_'+market+'.png',dpi=600, bbox_inches='tight')
+
+
+#######################################
+# Market factor: correlation and regression
+
+### Select the first window
+p = N
+n = int(2*p)
+print('p,n:', p, n)
+R = returns.iloc[:n, 0:p]
+R_sample = preproceso(R)
+n, p = R_sample.shape
+R_market = preproceso(returns_market.iloc[:n])
+
+
+### Eigenvalues and eigenvectors
+q = p/n
+t, marcenko = MP(q)
+E = R_sample.corr()
+eigval_E, eigvec_E = eigh(E)
+
+
+U_p = np.abs(eigvec_E[:, -1])
+G_p = np.zeros([n])
+for i in range(p):
+    G_p += U_p[i]*R_sample.iloc[:, i]
+G_p = G_p/G_p.std()
+G_p.name = index
+
+
+
+
+plt.figure()
+length, period, dates = axis(252,R_market)
+plt.plot(G_p.values,label='Market')
+plt.plot(R_market.values, label=index)
+plt.xticks(np.arange(0, length, period),dates) 
+plt.xticks(fontsize=10, rotation=90)
+plt.legend()
+plt.savefig(path+'/figures/market_'+market+'.png', dpi=600, bbox_inches='tight')
+
+
+coef = np.corrcoef(R_market, G_p)[0, 1]
+print('corr coef: ', coef)
+
+
+mod = sm.OLS(R_market.values, sm.add_constant(G_p, prepend=True).values)
+regression = mod.fit()
+print(regression.summary())
+
+X_plot = np.linspace(-7, 5, 100)
+plt.figure()
+plt.scatter(G_p, R_market)
+plt.plot(X_plot, X_plot * regression.params[1] + regression.params[0], '--k')
+plt.xlabel(index)
+plt.ylabel('Market')
+plt.savefig(path+'/figures/regression_'+market+'.png',
+            dpi=600, bbox_inches='tight')
+
+
+R_residuals = R_sample.subtract(G_p, axis='index')
+E_residuals = R_residuals.corr()
+E_list_residuals = list(np.sort(E_residuals.values.flatten()))
+del E_list_residuals[-p:]
+
+E_list = list(np.sort(E.values.flatten()))
+del E_list[-p:]
+
+plt.figure()
+sns.histplot(E_list, stat="density", element="step", fill=False, label='C')
+sns.histplot(E_list_residuals, stat="density", element="step",
+              fill=False, label='C (market removed)')
+plt.legend()
+plt.savefig(path+'/figures/fig10_Stanley2002_'+market+'.png', dpi=600, bbox_inches='tight')
+
+
+
+### Marchenko-Pastur
+plt.figure()
+q=1/2
+t, marcenko = MP(q)
+plt.plot(t,marcenko,label='Marcenko-Pastur: q=1/2') 
+q=1/10
+t, marcenko = MP(q)
+plt.plot(t,marcenko,label='Marcenko-Pastur: q=1/10') 
+q=1/100
+t, marcenko = MP(q)
+plt.plot(t,marcenko,label='Marcenko-Pastur: q=1/100') 
+#sns.histplot(data=eigval_E[:-1],stat="density", element="step", fill=False,kde=True, label='$\lambda$')
+plt.legend()
+plt.tight_layout()
+plt.savefig(path + '/figures/MP_'+market+'.png',dpi=600, bbox_inches='tight')
+plt.show()
+
+
+#######################################
+# Ventanas moviles
+print('moving window analysis:', market, case)
+#######################################
+
+if (case == 'high') and (market== 'sp500'):
+    p_i = N
+    q = 1/2
+    m = 185
+    step = 8
+
+if (case == 'high') and (market== 'nasdaq'):
+    p_i = N
+    q = 1/2
+    m = 114
+    step = 8
+    
+elif (case == 'low') and (market== 'sp500'):
+    p_i = N
+    q = 1/8
+    m = 60
+    step = 4
+
+elif (case == 'low') and (market== 'nasdaq'):
+    p_i = 450
+    q = 1/8
+    m = 47
+    step = 5
+
+
+else:
+    pass
+
+n_i = int((1/q)*p_i)
+delta = 20  
+k1_k0 = 8
+
+dates_in = []
+number_signals_E = []
+number_signals_tw_a1 = []
+number_signals_tw_a5 = []
+number_signals_tw_a10 = []
+TABLA_R = pd.DataFrame(columns=['null'])
+for i in range(m):
+
+    
+    print(i)
+    
+    start = i*delta
+    end = i*delta + n_i
+
+    R_window_in = preproceso(returns.iloc[start:end, :p_i])
+    if market=='sp500':
+        dates_in.append(R_window_in.index[-1].strftime("%Y-%m-%d"))
+    else:
+        dates_in.append(R_window_in.index[-1])        
+
+
+    # Tracy-Widom
+    E = R_window_in.corr()
+    count_E, XI_E = clipping_B(E, n_i)  # Empirical
+    number_signals_E.append(count_E)
+    eigval_E, eigvec_E = eigh(E)
+    number_signals_tw_a1.append(test_tw(eigval_E, n_i, alpha = 0.01))
+    number_signals_tw_a5.append(test_tw(eigval_E, n_i, alpha = 0.05))
+    number_signals_tw_a10.append(test_tw(eigval_E, n_i, alpha = 0.10))
+
+
+    # Onatski
+    R = np.zeros([k1_k0])
+    for j in range(k1_k0):
+        R[j] = onatski(R_window_in, j)
+    R_statistics = pd.DataFrame(R, columns=[str(i)])
+    TABLA_R = pd.concat([TABLA_R, R_statistics], axis=1)
+TABLA_R = TABLA_R.drop(columns=['null'])
+
+
+# Number of factors
+NUMBER_FACTORS_ONATSKI_l1 = []
+NUMBER_FACTORS_ONATSKI_l5 = []
+NUMBER_FACTORS_ONATSKI_l10 = []
+for i in range(m):
+    NUMBER_FACTORS_ONATSKI_l1.append(test_onatski(TABLA_R, j=i, level=1))
+    NUMBER_FACTORS_ONATSKI_l5.append(test_onatski(TABLA_R,  j=i, level=5))
+    NUMBER_FACTORS_ONATSKI_l10.append(test_onatski(TABLA_R,  j=i, level=10))
+
+
+number_factors = pd.DataFrame(list(zip(NUMBER_FACTORS_ONATSKI_l1, NUMBER_FACTORS_ONATSKI_l5,\
+                                       NUMBER_FACTORS_ONATSKI_l10)),columns =['1%', '5%','10%'])
+
+number_components = pd.DataFrame(list(zip(number_signals_tw_a1, number_signals_tw_a5,\
+                    number_signals_tw_a10,number_signals_E)),columns =['1%', '5%','10%','upper'])
+#######################################
+### plot and save figures
+
+# number of factors
+plt.figure()
+x = np.linspace(0, m-1, m)
+plt.axhline(y=k1_k0, color='r', linestyle='--')
+plt.scatter(x, number_factors['1%'], marker="o",label='$\\alpha = 0.01$')
+plt.scatter(x,number_factors['5%'], marker="^", label='$\\alpha = 0.05$')
+plt.scatter(x,number_factors['10%'],marker="*", label='$\\alpha = 0.10$')
+plt.xticks(np.arange(0, m, step), dates_in[::step])
+plt.xticks(rotation=90)
+plt.grid()
+plt.legend()
+plt.savefig(path+'/figures/Number_Factors_Onatski_'+market+'_'+case+'.png', dpi=600, bbox_inches='tight')
+plt.show()
+
+
+# number of components
+plt.figure()
+x = np.linspace(0, m-1, m)
+plt.scatter(x,number_components['1%'], marker="o",label='$\\alpha = 0.01$')
+plt.scatter(x,number_components['5%'], marker="^", label='$\\alpha = 0.05$')
+plt.scatter(x,number_components['10%'],marker="*", label='$\\alpha = 0.10$')
+plt.scatter(x, number_components['upper'], marker="s", label='$\lambda_{+}$')
+plt.xticks(np.arange(0, m, step), dates_in[::step])
+plt.xticks(rotation=90)
+plt.grid()
+plt.legend()
+plt.savefig(path+'/figures/Number_Components_'+market+'_'+case+'.png', dpi=600, bbox_inches='tight')
+plt.show()
+
+
+
+#######################################
+### save files
+export_csv = TABLA_R.to_csv(path+'/files/Tabla_R_'+market+'_'+case+'.csv', header=True)
+export_csv = number_factors.to_csv(path+'/files/number_factors_'+market+'_'+case+'.csv', header=True)
+export_csv = number_components.to_csv(path+'/files/number_components_'+market+'_'+case+'.csv', header=True)
+
+dates = list(dates_in)
+if market=='sp500':
+    dates.insert(0,returns.index[0].strftime("%Y-%m-%d"))
+else:
+    dates.insert(0,returns.index[0])
+export_csv = pd.DataFrame(dates).to_csv(path+'/files/dates_range_'+market+'_'+case+'.csv', header=True)
+export_csv = pd.DataFrame(R_window_in.columns.tolist()).to_csv(path+'/files/tickers_'+market+'_'+case+'.csv', header=True)
+
+
+
+
+end_time = time.time()
+elapsed = end_time - start_time
+print(elapsed)
+
+
+
+parameters = {'market': market, 'case':case,'elapsed':elapsed,\
+              'percentage':percentage, 'start_date':start_date,\
+              'q':q, 'm':m, 'step':step, 'T':T,'N':N,'p_i':p_i,\
+              'n_i':n_i,'delta':delta,'k1_k0':k1_k0}
+out_file = open(path + '/files/parameters_'+str(market)+'_'+str(case)+'.json', "w") 
+json.dump(parameters, out_file, indent = 6) 
+out_file.close() 
